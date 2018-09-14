ae657e7151fcb5380e0ca6283344db845ba00893
Max Melzer <max.melzer@netresearch.de>
2018-09-14 16:34:05 +0200

    PDAC-4: Rename project and modify license headers

---
+++ src/app/code/community/Postdirekt/Address/Helper/Data.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+
+/**
+ * Class Postdirekt_Address_Helper_Data
+ *
+ * @package   Postdirekt Address
+ * @author    Sebastian Ertner <sebastian.ertner@netresearch.de>
+ * @copyright 2018 Netresearch GmbH & Co. KG
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/
+ */
+class Postdirekt_Address_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
--- src/app/code/community/Postdirekt/Address/Model/Config.php
+++ src/app/code/community/Postdirekt/Address/Model/Config.php
@@ -1,32 +1,22 @@
 <?php
 /**
- * Deutschepost Autocomplete
+ * See LICENSE.md for license details.
+ */
+
+
+/**
+ * Class Postdirekt_Address_Model_Config
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade this extension to
- * newer versions in the future.
- *
- * PHP version 5
- *
- * @category  Deutschepost
- * @package   Deutschepost Autocomplete
+ * @package   Postdirekt Address
  * @author    Sebastian Ertner <sebastian.ertner@netresearch.de>
  * @copyright 2018 Netresearch GmbH & Co. KG
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      http://www.netresearch.de/
  */
-class Deutschepost_Autocomplete_Model_Config
+class Postdirekt_Address_Model_Config
 {
     const CONFIG_SECTION = 'customer';
-    const CONFIG_GROUP = 'deutschepost_autocomplete';
+    const CONFIG_GROUP = 'postdirekt_address';
 
 
     const CONFIG_XML_FIELD_ENABLED = 'enabled';
