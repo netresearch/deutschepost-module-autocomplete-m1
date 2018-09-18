c6f5260384754afb7842590be59c13acafead39e
Andreas Müller <andreas.mueller@netresearch.de>
2018-09-18 09:30:45 +0200

    PDAC-22: intial AutocompleteController

---
+++ src/app/code/community/Postdirekt/Autocomplete/controllers/AutocompleteController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+
+/**
+ * Class Postdirekt_Address_AutocompleteController
+ *
+ * @package   Postdirekt Address
+ * @author    Andreas Müller <andreas.mueller@netresearch.de>
+ * @copyright 2018 Netresearch GmbH & Co. KG
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/
+ */
+class Postdirekt_Address_AutocompleteController extends Mage_Core_Controller_Front_Action
+{
+    public function searchAction()
+    {
+
+    }
+
+    public function selectAction()
+    {
+
+    }
+}
