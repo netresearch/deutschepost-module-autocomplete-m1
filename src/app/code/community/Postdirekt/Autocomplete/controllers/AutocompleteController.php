2856fe05f7d66840aab3d1a231f390d533fe44fb
Andreas Müller <andreas.mueller@netresearch.de>
2018-09-19 15:48:19 +0200

    PDAC-8: rename Autocomplete controller

--- src/app/code/community/Postdirekt/Autocomplete/controllers/AutocompleteController.php
+++ src/app/code/community/Postdirekt/Autocomplete/controllers/AutocompleteController.php
@@ -12,11 +12,11 @@
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      http://www.netresearch.de/
  */
-class Postdirekt_Address_AutocompleteController extends Mage_Core_Controller_Front_Action
+class Postdirekt_Autocomplete_AutocompleteController extends Mage_Core_Controller_Front_Action
 {
     public function searchAction()
     {
-
+        $requestData = $this->getRequest()->getParams();
     }
 
     public function selectAction()
