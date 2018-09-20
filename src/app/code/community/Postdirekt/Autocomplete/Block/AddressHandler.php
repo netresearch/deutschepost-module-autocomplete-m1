4e7cbc06b11558625a98f13ce0580cccc4a037c0
Mathias Uhlmann <mathias.uhlmann@netresearch.de>
2018-09-20 18:26:06 +0200

    PDAC-24: add autocomplete js to checkout page

---
+++ src/app/code/community/Postdirekt/Autocomplete/Block/AddressHandler.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+
+/**
+ * Class Postdirekt_Autocomplete_Block_AddressHandler
+ *
+ * @package   Postdirekt\Autocomplete\Block
+ * @author    Mathias Uhlmann <mathias.uhlmann@netresearch.de>
+ * @copyright 2018 Netresearch GmbH & Co. KG
+ * @license   https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://www.netresearch.de/
+ */
+class Postdirekt_Autocomplete_Block_AddressHandler extends Mage_Core_Block_Template
+{
+    public function _construct()
+    {
+    }
+}
---
+++ src/js/postdirekt_autocomplete/autocomplete-handler.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var AddressAutocomplete = Class.create();
+
+/**
+ * ToDo: tbd
+ */
+AddressAutocomplete.prototype = {
+    /**
+     * @constructor
+     */
+    initialize: function(apiUrl, formId) {
+
+    }
+};
