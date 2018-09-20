4525ccb2e219b07d9b0501f77fda1a044d0efefd
Andreas Müller <andreas.mueller@netresearch.de>
2018-09-20 16:40:45 +0200

    PDAC-22: update autocomplete controller

--- src/app/code/community/Postdirekt/Autocomplete/controllers/AutocompleteController.php
+++ src/app/code/community/Postdirekt/Autocomplete/controllers/AutocompleteController.php
@@ -15,6 +15,39 @@
 class Postdirekt_Autocomplete_AutocompleteController extends Mage_Core_Controller_Front_Action
 {
     /**
+     * @var Postdirekt_Autocomplete_Model_Webservice_ServiceClientFactory
+     */
+    protected $clientFactory;
+
+    /**
+     * @var Postdirekt_Autocomplete_Model_Webservice_SearchRequestFactory
+     */
+    protected $searchRequestFactory;
+
+    /**
+     * @var Postdirekt_Autocomplete_Model_Webservice_SelectRequestFactory
+     */
+    protected $selectRequestFactory;
+
+    /**
+     * Postdirekt_Autocomplete_AutocompleteController constructor.
+     * @param Zend_Controller_Request_Abstract $request
+     * @param Zend_Controller_Response_Abstract $response
+     * @param array $invokeArgs
+     */
+    public function __construct(
+        Zend_Controller_Request_Abstract $request,
+        Zend_Controller_Response_Abstract $response,
+        array $invokeArgs = array()
+    ) {
+        $this->clientFactory = Mage::getModel('postdirekt_autocomplete/webservice_serviceClientFactory');
+        $this->searchRequestFactory = Mage::getModel('postdirekt_autocomplete/webservice_searchRequestFactory');
+        $this->selectRequestFactory = Mage::getModel('postdirekt_autocomplete/webservice_selectRequestFactory');
+        parent::__construct($request, $response, $invokeArgs);
+    }
+
+
+    /**
      * perform search request
      * @throws Zend_Controller_Response_Exception
      */
@@ -22,14 +55,12 @@ class Postdirekt_Autocomplete_AutocompleteController extends Mage_Core_Controlle
     {
         $requestData = $this->getRequest()->getParams();
         try {
-            /** @var Postdirekt_Autocomplete_Model_Webservice_ServiceClientFactory $clientFactory */
-            $clientFactory = Mage::getModel('postdirekt_autocomplete/webservice_serviceClientFactory');
+            $storeCode = Mage::app()->getStore()->getCode();
+            $args = array('scope' => $storeCode);
             /** @var Postdirekt_Autocomplete_Model_Webservice_ServiceClient $client */
-            $client = $clientFactory->create($this->getStoreCode());
+            $client = $this->clientFactory->create($args);
 
-            /** @var Postdirekt_Autocomplete_Model_Webservice_SearchRequestFactory $searchRequestFactory */
-            $searchRequestFactory = Mage::getModel('postdirekt_autocomplete/webservice_searchRequestFactory');
-            $searchRequest = $searchRequestFactory->create($requestData);
+            $searchRequest = $this->searchRequestFactory->create($requestData);
             $response = $client->search($searchRequest);
             $addresses = $response->getAddresses();
             $jsonResponse = Mage::helper('core/data')->jsonEncode($addresses);
@@ -48,25 +79,16 @@ class Postdirekt_Autocomplete_AutocompleteController extends Mage_Core_Controlle
     public function selectAction()
     {
         $requestData = $this->getRequest()->getParams();
-
         try {
-            /** @var Postdirekt_Autocomplete_Model_Webservice_ServiceClientFactory $clientFactory */
-            $clientFactory = Mage::getModel('postdirekt_autocomplete/webservice_serviceClientFactory');
+
             /** @var Postdirekt_Autocomplete_Model_Webservice_ServiceClient $client */
-            $client = $clientFactory->create($this->getStoreCode());
-            /** @var Postdirekt_Autocomplete_Model_Webservice_SelectRequestFactory $selectRequestFactory */
-            $selectRequestFactory = Mage::getModel('postdirekt_autocomplete/webservice_selectRequestFactory');
-            $selectRequest = $selectRequestFactory->create($requestData);
+            $client = $this->clientFactory->create($this->getStoreCode());
+            $selectRequest = $this->selectRequestFactory->create($requestData);
             $client->select($selectRequest);
+
             $this->getResponse()->setHttpResponseCode(200);
         } catch (Exception $exception) {
             $this->getResponse()->setHttpResponseCode($exception->getCode());
         }
-    }
-
-    protected function getStoreCode()
-    {
-        $storeCode = Mage::app()->getStore()->getCode();
-        return array('scope' => $storeCode);
     }
 }
