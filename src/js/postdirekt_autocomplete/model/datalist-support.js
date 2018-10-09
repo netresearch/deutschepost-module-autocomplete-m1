37f60940a64a43cc0a0483b083ecd31a9cd09b96
Rico Sonntag <rico.sonntag@netresearch.de>
2018-10-09 13:22:06 +0200

    PDAC-41: Use polyfill for firefox

--- src/js/postdirekt_autocomplete/model/datalist-support.js
+++ src/js/postdirekt_autocomplete/model/datalist-support.js
@@ -2,19 +2,32 @@
 
 var DatalistSupport = Class.create();
 
+/**
+ * @type {{hasSupport: DatalistSupport.hasSupport}}
+ */
 DatalistSupport.prototype = {
-    initialize: function () {
+    /**
+     * @constructor
+     */
+    initialize: function () {},
 
-    },
+    /**
+     * Detect support for the 'datalist' html element
+     *
+     * @return {boolean}
+     */
     hasSupport: function () {
         var datalistSupported = !!(document.createElement('datalist') && window.HTMLDataListElement);
-        var ua = navigator.userAgent;
+        var ua                = navigator.userAgent;
+        var isFireFox         = /firefox/i.test(ua);
 
         // Android does not have actual support
         var isAndroidBrowser = ua.match(/Android/) && !ua.match(/(Firefox|Chrome|Opera|OPR)/);
-        if( datalistSupported && !isAndroidBrowser ) {
+
+        if (datalistSupported && !isAndroidBrowser && !isFireFox) {
             return true;
         }
+
         return false;
     }
 };
